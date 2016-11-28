1bded6e8ef075955df9411b5c57a9e9a74a9453a
jflav <jflavio1@hotmail.com>
2016-11-27 21:16:02 -0500

    adding wifiScan

--- wificonnector/src/main/java/com/jflavio1/wificonnector/WifiConnector.java
+++ wificonnector/src/main/java/com/jflavio1/wificonnector/WifiConnector.java
@@ -4,11 +4,20 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
 import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -19,6 +28,7 @@ import java.util.List;
 
 /**
  * WifiConnector object should be created inside service or intentService
+ * only works for configuredNetworks that has been created by this app
  */
 public class WifiConnector {
 
@@ -47,51 +57,83 @@ public class WifiConnector {
      */
     private ConnectionResultListener connectionResultListener;
 
+    private ShowWifiListener showWifiListener;
+
     /**
      * intent filter to listen for wifi state
      */
-    private IntentFilter mIntentFilter;
+    private IntentFilter chooseWifiFilter;
 
     /**
-     * inherits from Broadcast receiver, will listen for {@link WifiConnector#mIntentFilter}
+     * filter for showing wifiList
+     */
+    private IntentFilter showWifiListFilter;
+
+    /**
+     * inherits from Broadcast receiver, will listen for {@link WifiConnector#chooseWifiFilter}
      */
     private WifiReceiver receiverWifi;
+
+    /**
+     * broadcast receiver for showing wifiList
+     */
+    private ShowoWifiReceiver showWifiReceiver;
 
     /**
      * static object for getting all wifi list that were configured by your app
      */
     private static List<WifiConfiguration> confList;
 
-    public static final int AUTHENTICATION_ERROR = 111;
-    public static final int NOT_FOUND_ERROR      = 222;
-    public static final int SAME_NETWORK         = 444;
+    /**
+     * codes for choosewifi
+     */
+    public static final int SUCCESS_CONNECTION = 2500;
+    public static final int AUTHENTICATION_ERROR = 2501;
+    public static final int NOT_FOUND_ERROR = 2502;
+    public static final int SAME_NETWORK = 2503;
+    public static final int UNKOWN_ERROR = 2504;
+
+    /**
+     * codes for searching wifi
+     */
+    public static final int WIFI_NETWORKS_SUCCESS_FOUND = 2600;
+    public static final int NO_WIFI_NETWORKS = 2601;
+
+    /**
+     * WIFI SECURITY TYPES
+     */
+    private static final String SECURITY_WEP   = "WEP";
+    private static final String SECURITY_WPA   = "WPA";
+    private static final String SECURITY_PSK   = "PSK";
+    private static final String SECURITY_EAP   = "EAP";
 
     /**
      * for setting wifi access point security type
      */
-    public static final String SECURITY_NONE            = "NONE";
+    public static final String SECURITY_NONE = "NONE";
 
     public WifiConnector(Context context) {
         this.context = context;
         wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        enableWifi();
     }
 
     public WifiConnector(WifiConfiguration wifiConfiguration, Context context) {
+        this.wifiConfiguration = wifiConfiguration;
         this.context = context;
         this.wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        this.wifiConfiguration = wifiConfiguration;
-
+        enableWifi();
     }
 
-    public WifiConnector(Context context, String SSID, @Nullable String BSSID, String securityType, @Nullable String password){
+    public WifiConnector(Context context, String SSID, @Nullable String BSSID, String securityType, @Nullable String password) {
         this.context = context;
         this.wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         this.wifiConfiguration = new WifiConfiguration();
         this.wifiConfiguration.SSID = SSID;
         this.wifiConfiguration.BSSID = BSSID;
-        if(securityType.equals(SECURITY_NONE)){
+        if (securityType.equals(SECURITY_NONE)) {
             wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-        }else{
+        } else {
             wifiConfiguration.preSharedKey = ssidFormat(password);
             wifiConfiguration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
             wifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.WPA); // For WPA
@@ -103,41 +145,87 @@ public class WifiConnector {
             wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
             wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
         }
+
+        enableWifi();
     }
 
-    public boolean setPriority(int priority){
-        try{
+    public void enableWifi(){
+        if(!wifiManager.isWifiEnabled()) {
+            wifiLog("Wifi was not enable, enabling it...");
+            wifiManager.setWifiEnabled(true);
+        }
+    }
+
+    public boolean setPriority(int priority) {
+        try {
             this.wifiConfiguration.priority = priority;
             return true;
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             return false;
         }
     }
 
-    private boolean isAlreadyConnected(String SSID){
-        if(wifiManager.getConnectionInfo().getSSID().equals(SSID)){
-            return true;
+    private void createChooseWifiBroadcastListener() {
+        chooseWifiFilter = new IntentFilter();
+        chooseWifiFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
+        receiverWifi = new WifiReceiver();
+        try {
+            this.context.registerReceiver(receiverWifi, chooseWifiFilter);
+        } catch (Exception e) {
+            wifiLog("Register broadcast error (Choose): " + e.toString());
+        }
+    }
+
+    private void createShowWifiBroadcastListener() {
+        showWifiListFilter = new IntentFilter();
+        showWifiListFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        showWifiReceiver = new ShowoWifiReceiver();
+        try {
+            this.context.registerReceiver(showWifiReceiver, showWifiListFilter);
+        } catch (Exception e) {
+            wifiLog("Register broadcast error (ShowWifi): " + e.toString());
+        }
+    }
+
+    public void showWifiList(ShowWifiListener showWifiListener) {
+        this.showWifiListener = showWifiListener;
+        createShowWifiBroadcastListener();
+        scanWifiNetworks();
+    }
+
+    private void scanWifiNetworks() {
+        wifiManager.startScan();
+    }
+
+    public static JSONArray wifiResultToJsonArray(List<ScanResult> wifiScanResult) throws JSONException {
+        return new JSONArray(wifiScanResult.toString());
+    }
+
+    private boolean isAlreadyConnected(String BSSID){
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getActiveNetworkInfo();
+
+        if(mWifi.getType() == ConnectivityManager.TYPE_WIFI && mWifi.isConnected()) {
+            if (wifiManager.getConnectionInfo().getBSSID() != null &&
+                    wifiManager.getConnectionInfo().getBSSID().equals(BSSID)) {
+                wifiLog("Already connected to: " + wifiManager.getConnectionInfo().getSSID());
+                return true;
+            }
         }
         return false;
     }
 
-    private void createBroadcastListener(){
-        mIntentFilter = new IntentFilter();
-        mIntentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
-        receiverWifi = new WifiReceiver();
-        try{this.context.registerReceiver(receiverWifi, mIntentFilter);}catch (Exception e){wifiLog("Register broadcast error: " + e.toString());}
-    }
-
     /**
      * tries to connect to specific wifi
+     *
      * @param connectionResultListener with methods of success and error
      */
-    public void connectToWifi(ConnectionResultListener connectionResultListener){
+    public void connectToWifi(ConnectionResultListener connectionResultListener) {
         this.connectionResultListener = connectionResultListener;
-        if(isAlreadyConnected(quoteFormat(wifiConfiguration.SSID))){
+        if(isAlreadyConnected(wifiConfiguration.BSSID)){
             connectionResultListener.errorConnect(SAME_NETWORK);
         }else {
-            createBroadcastListener();
+            createChooseWifiBroadcastListener();
             connectToWifi();
             wifiManager.reconnect();
         }
@@ -147,14 +235,15 @@ public class WifiConnector {
      * allows to connect to specific Wifi access point
      * tries to get network id
      * if network id = -1 add network configuration
+     *
      * @return boolean value if connection was successfuly complete
      */
-    private boolean connectToWifi(){
+    private boolean connectToWifi() {
         int networkId = getNetworkId(wifiConfiguration.SSID);
-        wifiLog("network id: " + networkId);
-        if(networkId == -1){
+        wifiLog("network id found: " + networkId);
+        if (networkId == -1) {
             networkId = wifiManager.addNetwork(wifiConfiguration);
-            wifiLog("networkId now: "+networkId);
+            wifiLog("networkId now: " + networkId);
         }
         return enableNetwork(networkId);
     }
@@ -162,6 +251,7 @@ public class WifiConnector {
     /**
      * search network id by given SSID
      * if network is totally new, it returns -1
+     *
      * @param SSID name of wifi network
      * @return wifi network id
      */
@@ -169,7 +259,7 @@ public class WifiConnector {
         confList = wifiManager.getConfiguredNetworks();
         if (confList != null && confList.size() > 0) {
             for (WifiConfiguration existingConfig : confList) {
-                if (quoteFormat(existingConfig.SSID).equals(quoteFormat(SSID))) {
+                if (trimQuotes(existingConfig.SSID).equals(trimQuotes(SSID))) {
                     return existingConfig.networkId;
                 }
             }
@@ -178,7 +268,7 @@ public class WifiConnector {
     }
 
     private boolean enableNetwork(int networkId) {
-        if(networkId == -1){
+        if (networkId == -1) {
             wifiLog("So networkId still -1, there was an error... may be authentication?");
             connectionResultListener.errorConnect(AUTHENTICATION_ERROR);
             context.unregisterReceiver(receiverWifi);
@@ -188,69 +278,83 @@ public class WifiConnector {
     }
 
     private boolean connectWifiManager(int networkId) {
+        wifiManager.disconnect();
         return wifiManager.enableNetwork(networkId, true);
     }
 
-    private static String ssidFormat(String str){
-        if(!str.isEmpty()) {
+    public static String ssidFormat(String str) {
+        if (!str.isEmpty()) {
             return "\"" + str + "\"";
         }
         return str;
     }
 
-    private static String quoteFormat(String str) {
+    private static String trimQuotes(String str) {
         if (!str.isEmpty()) {
             return str.replaceAll("^\"*", "").replaceAll("\"*$", "");
         }
         return str;
     }
 
-    public interface ConnectionResultListener{
+    public interface ConnectionResultListener {
         void successfulConnect();
         void errorConnect(int codeReason);
     }
 
-    private void deleteWifiConf() {
+    public interface ShowWifiListener {
+        void onNetworksFound(List<ScanResult> wifiList);
+        void errorSearchingNetworks(int errorCode);
+    }
+
+    private boolean deleteWifiConf() {
         try {
             confList = wifiManager.getConfiguredNetworks();
             for (WifiConfiguration i : confList) {
                 if (i.SSID != null && i.SSID.equals(ssidFormat(wifiConfiguration.SSID))) {
                     wifiLog("Deleting wifi configuration: " + i.SSID);
                     wifiManager.removeNetwork(i.networkId);
-                    wifiManager.saveConfiguration();
+                    return wifiManager.saveConfiguration();
                 }
             }
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+            return false;
+        }
+        return false;
     }
 
-    private void wifiLog(String text){
-        Log.d(TAG, text);
+    private void wifiLog(String text) {
+        Log.d(TAG, "WifiConnector: " + text);
     }
 
     private class WifiReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context c, Intent intent) {
-            String action  = intent.getAction();
-            if(action.equals(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION)){
+            String action = intent.getAction();
+            if (action.equals(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION)) {
 
                 SupplicantState state = intent.getParcelableExtra(WifiManager.EXTRA_NEW_STATE);
 
-                switch (state){
+                wifiLog("Broadcast action: " + state);
+
+                switch (state) {
                     case COMPLETED:
                         String bssid = intent.getStringExtra(WifiManager.EXTRA_BSSID);
                         wifiLog("Connection to Wifi was successfuly completed...\n" +
-                                "Connected to bssid: " + bssid );
+                                "Connected to bssid: " + bssid);
                         connectionResultListener.successfulConnect();
                         context.unregisterReceiver(receiverWifi);
                         break;
                     case DISCONNECTED:
-                        wifiLog("Disconnected...");
                         int supl_error = intent.getIntExtra(WifiManager.EXTRA_SUPPLICANT_ERROR, -1);
-                        if(supl_error == WifiManager.ERROR_AUTHENTICATING){
+                        wifiLog("Disconnected... Supplicant error: " + supl_error);
+                        if (supl_error == WifiManager.ERROR_AUTHENTICATING ) {
                             wifiLog("Authentication error...");
+                            if(deleteWifiConf()) {
+                                connectionResultListener.errorConnect(AUTHENTICATION_ERROR);
+                            }else{
+                                connectionResultListener.errorConnect(UNKOWN_ERROR);
+                            }
                             context.unregisterReceiver(receiverWifi);
-                            connectionResultListener.errorConnect(AUTHENTICATION_ERROR);
-                            deleteWifiConf();
                         }
                         break;
                     case AUTHENTICATING:
@@ -258,9 +362,30 @@ public class WifiConnector {
                         break;
                 }
 
-
             }
         }
     }
 
+    private class ShowoWifiReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            List<ScanResult> wifiScanResult = wifiManager.getScanResults();
+            showWifiListener.onNetworksFound(wifiScanResult);
+            context.unregisterReceiver(this);
+        }
+
+        private String getWifiSecurityType(ScanResult result) {
+            if (result.capabilities.contains("WEP")) {
+                return SECURITY_WEP;
+            } else if (result.capabilities.contains("WPA")) {
+                return SECURITY_WPA;
+            } else if (result.capabilities.contains("PSK")) {
+                return SECURITY_PSK;
+            } else if (result.capabilities.contains("EAP")) {
+                return SECURITY_EAP;
+            }
+            return SECURITY_NONE;
+        }
+
+    }
 }
