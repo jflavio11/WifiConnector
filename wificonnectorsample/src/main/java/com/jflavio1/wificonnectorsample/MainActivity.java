dd79509c78f348c8d057c64092c2dd68ee8347df
JoseFlavio <jflavio90@gmail.com>
2017-10-18 13:40:47 -0500

    added sample project

---
+++ wificonnectorsample/src/main/java/com/jflavio1/wificonnectorsample/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Created by Jose Flavio on 10/18/17 1:20 PM.
+ * Copyright (c) 2017 JoseFlavio.
+ * All rights reserved.
+ */
+
+package com.jflavio1.wificonnectorsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ wificonnectorsample/src/test/java/com/jflavio1/wificonnectorsample/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+/*
+ * Created by Jose Flavio on 10/18/17 1:20 PM.
+ * Copyright (c) 2017 JoseFlavio.
+ * All rights reserved.
+ */
+
+package com.jflavio1.wificonnectorsample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
