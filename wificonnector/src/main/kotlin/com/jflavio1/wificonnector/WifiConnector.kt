3844fab2664fead83a9422e332e69bdd87780a80
Jose Flavio <jflavio90@gmail.com>
2021-02-12 20:42:23 -0500

    update gradle config

---
+++ wificonnector/src/main/kotlin/com/jflavio1/wificonnector/WifiConnector.kt
@@ -0,0 +1,16 @@
+/*
+ * Created by Jose Flavio on 2/12/21 8:49 PM.
+ * Copyright (c) 2017 JoseFlavio.
+ * All rights reserved.
+ */
+
+package com.jflavio1.wificonnector
+
+/**
+ * WifiConnector
+ *
+ * @author Jose Flavio - jflavio90@gmail.com
+ * @since  12/02/2021
+ */
+class WifiConnector2 {
+}
